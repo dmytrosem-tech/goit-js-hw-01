85e328c223763d15d36b6f0f749e3bf1da24958e
Dmytro Semeruk <deemoon83@gmail.com>
2021-08-31 21:29:19 +0300

    вв

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,41 @@
-const name = 'Генератор защитного поля';
-let price = 1000;
-console.log(`Выбран ${name}, цена за штуку ${price}`);
+// const name = 'Генератор защитного поля';
+// let price = 1000;
+// console.log(`Выбран ${name}, цена за штуку ${price}`);
 
-price = 2000;
-console.log(`Выбран ${name}, цена за штуку ${price}`);
+// price = 2000;
+// console.log(`Выбран ${name}, цена за штуку ${price}`);
+
+// const getUserAsync = async () => {
+//   try {
+//     const result = await fetch("http://jsonplaceholder.typicode.com/users");
+//     const dataFromServer = await result.json();
+//     return dataFromServer;
+//   } catch (error) {
+//     console.log("Error");
+//   }
+// };
+
+// console.log((getUserAsync()));
+// // getUserAsync().then((array) => console.log("arr", array));
+
+// function renderList () {
+//     console.log('Render list');
+// }
+
+// function renderButton () {
+//     console.log('Render Button');
+// }
+
+// const draw = async () => {
+//     await renderList()
+//     await renderButton()
+// }
+
+// const paint = () => {
+//     renderList();
+//     renderButton();
+// }
+
+// draw()
+// paint()
+
